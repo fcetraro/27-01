f71068e7bba0774ac0343c0778aa1fcf13d081f4
Federico Cetraro <>
2021-01-27 22:42:14 -0300

    Roman and Morse implementation

---
+++ src/main/java/com/example/demo/ParserController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ParserController {
+    @GetMapping("/parse/roman")
+    public String numberToRoman(@RequestParam(value = "value") int decimal){
+        RomanParser parser = new RomanParser();
+        return parser.intToRoman(decimal);
+    }
+    @GetMapping("/parse/morse")
+    public String morseToEnglish(@RequestParam(value = "value") String morse){
+        MorseParser parser = new MorseParser();
+        return parser.parseMorse(morse);
+    }
+}
